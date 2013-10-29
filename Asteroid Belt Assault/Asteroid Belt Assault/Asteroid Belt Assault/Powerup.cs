bb44af63d9d04fbedd89fc4302c41b754770e6ef
EpicMcWin <bankst42@gmail.com>
2013-10-29 08:48:32 -0400

    Updated

---
+++ Asteroid Belt Assault/Asteroid Belt Assault/Asteroid Belt Assault/Powerup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroid_Belt_Assault
+{
+    class Powerup
+    {
+        private Powerup shotgun = new Powerup();
+        private Powerup superSpin = new Powerup();
+        private Powerup uzi = new Powerup();
+        private Powerup nuke = new Powerup();
+        private Powerup invincible = new Powerup();
+
+        private void FireShot();
+        public Powerup shotgun
+        {
+            get
+            {
+                
+            }
+            set
+            {
+
+                if (keyState.IsKeyDown(Keys.Space))
+                {
+                    FireShot();
+                    FireShot();
+                    FireShot();
+                    FireShot();
+                }
+            }
+        }
+
+    }
+}
